84b488708a7f97a902bb309ef193d05a30a42866
Wadym_IH <glutter@live.ru>
2017-12-13 22:03:13 +0200

    parsed response from NP

--- pages/index.vue
+++ pages/index.vue
@@ -2,53 +2,14 @@
   <div class="page-container">
     <md-app>
       <md-app-toolbar class="md-primary">
-        <md-button class="md-icon-button" @click="toggleMenu" v-if="!menuVisible">
-          <md-icon>menu</md-icon>
-        </md-button>
         <span class="md-title">{{title}}</span>
+        <md-button class="md-raised md-accent" @click="getEN">Загрузить ЕН</md-button>
       </md-app-toolbar>
-
-      <md-app-drawer :md-active.sync="menuVisible" md-persistent="full">
-        <md-toolbar class="md-transparent" md-elevation="0">
-          <span>Navigation</span>
-
-          <div class="md-toolbar-section-end">
-            <md-button class="md-icon-button md-dense" @click="toggleMenu">
-              <md-icon>keyboard_arrow_left</md-icon>
-            </md-button>
-          </div>
-        </md-toolbar>
-
-        <md-list>
-          <md-list-item>
-            <md-icon>move_to_inbox</md-icon>
-            <span class="md-list-item-text">Inbox</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>send</md-icon>
-            <span class="md-list-item-text">Sent Mail</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>delete</md-icon>
-            <span class="md-list-item-text">Trash</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>error</md-icon>
-            <span class="md-list-item-text">Spam</span>
-          </md-list-item>
-        </md-list>
-      </md-app-drawer>
-
       <md-app-content>
-          <md-empty-state
-      md-icon="devices_other"
-      md-label="Create your first project"
-      md-description="Creating project, you'll be able to upload your design and collaborate with people.">
-      <md-button @click="getEN" class="md-primary md-raised">Загрузить ЕН</md-button>
-    </md-empty-state>
+        <div>
+            <li v-for="item in createdEn" v-bind:key="item.id">{{item.Weight}}</li>
+          </ul>
+        </div>
       </md-app-content>
     </md-app>
   </div>
@@ -61,45 +22,44 @@
     name: 'index',
     data: () => ({
       menuVisible: false,
-      title: 'NPSms'
+      title: 'NPSms',
+      createdEn: []
     }),
     methods: {
-      toggleMenu () {
+      toggleMenu() {
         this.menuVisible = !this.menuVisible
       },
-      getEN () {
+      getEN() {
         var url = 'https://api.novaposhta.ua/v2.0/json/'
         var apiKey = '6be5aca674dcb520b2b64a50c9f51935'
         var data = {
           "apiKey": apiKey,
-          "modelName": "Address",
-          "calledMethod": "searchSettlements",
+          "modelName": "InternetDocument",
+          "calledMethod": "getDocumentList",
           "methodProperties": {
-            "CityName": "згурівка",
-            "Limit": 5
+            "GetFullList": "1"
           }
         }
-        axios.post(url, data).then(function (response) {
-          console.log(response.data)
+        axios.post(url, data).then((response) => {
+          this.createdEn = response.data.data
+          console.log(this.createdEn)
         }).catch(function (error) {
           console.log(error)
         })
       }
     }
   }
+
 </script>
 
 <style lang="scss" scoped>
   .md-app {
     min-height: 100vh;
     border: 1px solid rgba(#000, .12);
-  }
-
-   // Demo purposes only
+  } // Demo purposes only
   .md-drawer {
     width: 230px;
     max-width: calc(100vw - 125px);
   }
+
 </style>
-
-
